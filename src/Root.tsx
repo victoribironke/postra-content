95e3b93af8772b637bc4e6bf36fa105587be5526
Victor Ibironke <victoribironke2019@gmail.com>
2026-02-11 12:11:32 +0100

    feat: add ShowcaseDemo composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,23 +2,31 @@ import "./index.css";
 import { Composition } from "remotion";
 import { HelloWorld, myCompSchema } from "./HelloWorld";
 import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
-
-// Each <Composition> is an entry in the sidebar!
+import {
+  ShowcaseVideo,
+  TOTAL_DURATION,
+} from "./Showcase/ShowcaseVideo";
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      {/* Showcase demo for designers */}
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render HelloWorld
+        id="ShowcaseDemo"
+        component={ShowcaseVideo}
+        durationInFrames={TOTAL_DURATION}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+
+      <Composition
         id="HelloWorld"
         component={HelloWorld}
         durationInFrames={150}
         fps={30}
         width={1920}
         height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
         schema={myCompSchema}
         defaultProps={{
           titleText: "Welcome to Remotion",
@@ -28,7 +36,6 @@ export const RemotionRoot: React.FC = () => {
         }}
       />
 
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
       <Composition
         id="OnlyLogo"
         component={Logo}
